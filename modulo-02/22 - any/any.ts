c77a90c17081872d5b2450dceac2bc1a0568ba8d
Vinícius Yoda <vyoda4604@gmail.com>
2025-08-11 19:56:54 -0300

    TS

---
+++ modulo-02/22 - any/any.ts
@@ -0,0 +1,13 @@
+const a: any = 34;
+const b: any = ['Glaucia'];
+
+const result = a + b;
+console.log(result);
+
+const formulario: { [campoFomulario: string]: any } = {
+  nome: 'Glaucia',
+  sobrenome: 'Lemos',
+  idade: 34,
+};
+
+console.log(formulario);
