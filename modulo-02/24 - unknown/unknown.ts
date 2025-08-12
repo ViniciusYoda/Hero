e1d95cbb59a9b5939bf9a527afdd74d2e547b6ef
Vinícius Yoda <vyoda4604@gmail.com>
2025-08-12 19:53:18 -0300

    tys

---
+++ modulo-02/24 - unknown/unknown.ts
@@ -0,0 +1,15 @@
+let valorVariavel: unknown;
+
+valorVariavel = true;
+valorVariavel = 123;
+valorVariavel = [];
+valorVariavel = 'Oi! Tudo bem?';
+
+let algumaCoisaAny: any;
+let algumaCoisaUnknown: unknown;
+
+console.log(algumaCoisaAny.toFixed());
+
+if (typeof algumaCoisaUnknown === 'number') {
+  console.log(algumaCoisaUnknown.toFixed());
+}
