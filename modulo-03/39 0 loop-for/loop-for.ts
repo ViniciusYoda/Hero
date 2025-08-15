a9e9f137bf8f41d8380a5b35e284a2caf0613177
Vinícius Yoda <vyoda4604@gmail.com>
2025-08-15 19:03:16 -0300

    ts

---
+++ modulo-03/39 0 loop-for/loop-for.ts
@@ -0,0 +1,14 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+const arrayNumeros = [10, 20, 30, 40, 50];
+
+for (const i of arrayNumeros) {
+  console.log(i);
+}
+
+const arrayNumeros_01 = [5, 4, 3, 2, 1, 0]
+
+const ordemNumerica: number[] = arrayNumeros_01.sort();
+console.log(ordemNumerica);
