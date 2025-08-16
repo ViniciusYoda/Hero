294ad12cedd192053647b7bc069953e04b9b7185
Vinícius Yoda <vyoda4604@gmail.com>
2025-08-16 11:18:08 -0300

    ts

---
+++ modulo-04/45 - optional-parameters/optional-parameters.ts
@@ -0,0 +1,35 @@
+export {};
+
+function informarDadosPessoa(idPessoa: number, nome: string, email?: string) {
+  console.log('Id Funcionário...: ', idPessoa, 'Nome...: ', nome);
+
+  if (email != undefined) console.log('E-mail...: ', email);
+}
+
+informarDadosPessoa(775544, 'Glaucia Lemos');
+informarDadosPessoa(994411, 'Jurema Lemos', 'jurema@example.com');
+
+function mensagemLog(mensagem: string, usuarioId?: number) {
+  const horaLog = new Date().toLocaleTimeString();
+
+  console.log(horaLog, mensagem, usuarioId || 'Usuário(a) não conectado(a)');
+}
+
+mensagemLog('Atualizar Página');
+mensagemLog('Usuário(a) logado(a) com sucesso');
+
+type Pessoa = {
+  idFuncionario: number;
+  nome: string;
+  idade?: number;
+  email?: string;
+};
+
+let pessoa: Pessoa;
+
+pessoa = {
+  idFuncionario: 112233,
+  nome: 'Glaucia Lemos',
+};
+
+console.log(pessoa);
