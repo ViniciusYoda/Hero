63837cc92cacab23861d29ebf810dedc7ec6fe4f
Vinícius Yoda <vyoda4604@gmail.com>
2025-08-27 19:20:49 -0300

    ts

---
+++ modulo-10/ts-node-express/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
